d436663f65d00e58f6303e4da99c5a7b76d07d60
James <James.Hacklander@gmail.com>
2021-04-17 10:48:20 -0700

    Added 5-day forcast to HTML. JS now feeds weather data to HTML.

--- assets/script.js
+++ assets/script.js
@@ -4,52 +4,76 @@ cityInput = document.getElementById("city");
 apiKey = "f9f66e4d8cd2e7c1f9cea3662563d53e"
 // var weatherData = {};
 
-var currentWeatherHeader = document.getElementById("current-weather")
-// currentIconEl = ?????
-var currentTempEl = document.getElementById("current-temp")
-var currentHumidityEl = document.getElementById("current-humidity")
-var currentWindSpeedEl= document.getElementById("wind-speed")
-var currentUvIndexEl = document.getElementById("uv-index")
+// var currentWeatherHeader = document.getElementById("current-weather")
+// // currentIconEl = ?????
+// var currentTempEl = document.getElementById("current-temp")
+// var currentHumidityEl = document.getElementById("current-humidity")
+// var currentWindSpeedEl= document.getElementById("wind-speed")
+// var currentUvIndexEl = document.getElementById("uv-index")
 //----------------------
 var futureDate1 = document.getElementById("future-date-1")
 var futureTemp1 = document.getElementById("future-temp-1")
 var futureHumidity1 = document.getElementById("future-humidity-1")
+// var lat = ""
+// var lon = ""
 
 
 
+citySearch.addEventListener("click", getLatLon);
 
-// citySearch.addEventListener("click", callApi);
-
-citySearch.addEventListener("click", function(event) {
-// function callApi(event) {
+// citySearch.addEventListener("click", function(event) {
+// Gets lat-lon data from Geocoding API for use in One Call API.
+function getLatLon(event) {
     // debugger;
     event.preventDefault();
     var cityInputValue = cityInput.value;
-    // Gets lat-lon data from Geocoding API for use in One Call API.
     fetch("http://api.openweathermap.org/geo/1.0/direct?q=" + cityInputValue + "&limit=1&appid=" + apiKey).then(function(response) {
         return response.json().then(function(latLonData) {
             var lat = latLonData[0].lat
             var lon = latLonData[0].lon
-            // Use One Call API with lat-lon input to get weather data in imperial units, excluding hourly and minute forcast data.
-            fetch("https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&units=imperial&exclude=hourly,minutely&appid=" + apiKey).then(function(weatherResponse) {
-                return weatherResponse.json().then(function(weatherData) {
-                    console.log(weatherData)
-                    // displayWeather(weatherData);
-                    console.log(weatherData.current.temp)
-                    console.log(Math.round(weatherData.current.temp))
-                    console.log(Math.round(weatherData.current.humidity))
-                    console.log(Math.round(weatherData.current.wind_speed))
-                    console.log(Math.round(weatherData.current.uvi))
-                    // ----------- FUTURE
-                    console.log(Math.round(weatherData.daily[0].temp.day))
-                    console.log(Math.round(weatherData.daily[0].humidity))
-
-                });
-            });
+            var cityName = latLonData[0].name
+            var stateName = latLonData[0].state
+            getWeather(lat, lon, cityName, stateName)
         });
     });
-});
+};
 
+
+// Use One Call API with lat-lon input to get weather data in imperial units, excluding hourly and minute forcast data.
+function getWeather(lat, lon, cityName, stateName) {
+    fetch("https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&units=imperial&exclude=hourly,minutely&appid=" + apiKey).then(function(weatherResponse) {
+    return weatherResponse.json().then(function(weatherData) {
+        console.log(weatherData)
+        // displayWeather(weatherData);
+        var currentDate = moment.unix(weatherData.current.dt).format("M/DD")
+        $("#current-weather").text(cityName +", " + stateName + " - " + currentDate)
+        $("#current-temp").text("Temperature: " + (Math.round(weatherData.current.temp)) + "°F")
+        $("#current-humidity").text("Humidity: " + (Math.round(weatherData.current.humidity)) + "%")
+        $("#wind-speed").text("Wind Speed: " + (Math.round(weatherData.current.wind_speed)) + " Mph")
+        $("#uv-index").text("UV Index: "+(Math.round(weatherData.current.uvi)))
+
+
+                    // console.log()
+                    // console.log(Math.round(weatherData.current.temp))
+                    // console.log(Math.round(weatherData.current.humidity))
+                    // console.log(Math.round(weatherData.current.wind_speed))
+                    // console.log()
+                    // ----------- FUTURE
+
+        for (i=1; i<6; i++) {
+            $("#future-date"+i).text(moment.unix(weatherData.daily[i].dt).format("M/DD"))
+            $("#future-icon"+i)
+            $("#future-temp"+i).text("Temperature: " + (Math.round(weatherData.daily[i].temp.day)) + "°F")
+            $("#future-humidity"+i).text("Humidity: " + (Math.round(weatherData.daily[i].humidity)) + "%")
+        }
+
+
+
+
+
+        });
+    });
+};
 
 
 
@@ -80,5 +104,5 @@ citySearch.addEventListener("click", function(event) {
 
 // Math.round()  to round temps or other variables
 // &exclude={part}
-// Use onecall API for UV.
+// Use onecall API for UV index.
 // Geocoding API for lat-long
