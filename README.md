7421eba03e419a13a90f73893f7d1e7de2613c03
James <James.Hacklander@gmail.com>
2021-04-17 21:41:08 -0700

    Added dates and icons. Added preview. Updated README.

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# Weather-Dashboard
-A page to search and view weather forcasts.
+ # Weather-Dashboard
+## A page to search and view weather forcasts.
+
+### Description:
+This page is designed to be a simple weather dashboard.
+There is a search form to enter your city.
+Upon entering a city into the search form the page will display current weather conditions on the right, and five-day forcast with summarized weather data below.
+
+### Acceptance criteria:
+- GIVEN a weather dashboard with form inputs
+- WHEN I search for a city
+- THEN I am presented with current and future conditions for that city and that city is added to the search history
+- WHEN I view current weather conditions for that city
+- THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index
+- WHEN I view the UV index
+- THEN I am presented with a color that indicates whether the conditions are favorable, moderate, or severe
+- WHEN I view future weather conditions for that city
+- THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, and the humidity
+- WHEN I click on a city in the search history
+- THEN I am again presented with current and future conditions for that city
+
+### Links:
+[Website preview](./assets/preview.png)<br>
+[Link to deployed website](https://monsaltus.github.io/Weather-Dashboard/)
+
+### Changelog:
+#### index.html:
+#### style.css:
+#### script.js:
--- assets/script.js
+++ assets/script.js
@@ -2,105 +2,64 @@
 citySearch = document.getElementById("city-search");
 cityInput = document.getElementById("city");
 apiKey = "f9f66e4d8cd2e7c1f9cea3662563d53e"
-// var weatherData = {};
 
-// var currentWeatherHeader = document.getElementById("current-weather")
-// // currentIconEl = ?????
-// var currentTempEl = document.getElementById("current-temp")
-// var currentHumidityEl = document.getElementById("current-humidity")
-// var currentWindSpeedEl= document.getElementById("wind-speed")
-// var currentUvIndexEl = document.getElementById("uv-index")
-//----------------------
-var futureDate1 = document.getElementById("future-date-1")
-var futureTemp1 = document.getElementById("future-temp-1")
-var futureHumidity1 = document.getElementById("future-humidity-1")
-// var lat = ""
-// var lon = ""
+citySearch.addEventListener("click", getWeather);
 
-
-
-citySearch.addEventListener("click", getLatLon);
-
-// citySearch.addEventListener("click", function(event) {
 // Gets lat-lon data from Geocoding API for use in One Call API.
-function getLatLon(event) {
+function getWeather(event) {
     // debugger;
     event.preventDefault();
     var cityInputValue = cityInput.value;
-    fetch("http://api.openweathermap.org/geo/1.0/direct?q=" + cityInputValue + "&limit=1&appid=" + apiKey).then(function(response) {
+    fetch("https://api.openweathermap.org/geo/1.0/direct?q=" + cityInputValue + "&limit=1&appid=" + apiKey).then(function(response) {
         return response.json().then(function(latLonData) {
             var lat = latLonData[0].lat
             var lon = latLonData[0].lon
             var cityName = latLonData[0].name
             var stateName = latLonData[0].state
-            getWeather(lat, lon, cityName, stateName)
+        // Use One Call API with lat-lon input to get weather data in imperial units, excluding hourly and minute forcast data.
+        fetch("https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&units=imperial&exclude=hourly,minutely&appid=" + apiKey).then(function(weatherResponse) {
+                return weatherResponse.json().then(function(weatherData) {
+                console.log(weatherData)
+                renderWeather(weatherData, cityName, stateName)
+                });
+            });
         });
     });
 };
 
-
-// Use One Call API with lat-lon input to get weather data in imperial units, excluding hourly and minute forcast data.
-function getWeather(lat, lon, cityName, stateName) {
-    fetch("https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&units=imperial&exclude=hourly,minutely&appid=" + apiKey).then(function(weatherResponse) {
-    return weatherResponse.json().then(function(weatherData) {
-        console.log(weatherData)
-        // displayWeather(weatherData);
-        var currentDate = moment.unix(weatherData.current.dt).format("M/DD")
-        $("#current-weather").text(cityName +", " + stateName + " - " + currentDate)
-        $("#current-temp").text("Temperature: " + (Math.round(weatherData.current.temp)) + "°F")
-        $("#current-humidity").text("Humidity: " + (Math.round(weatherData.current.humidity)) + "%")
-        $("#wind-speed").text("Wind Speed: " + (Math.round(weatherData.current.wind_speed)) + " Mph")
-        $("#uv-index").text("UV Index: "+(Math.round(weatherData.current.uvi)))
-
-
-                    // console.log()
-                    // console.log(Math.round(weatherData.current.temp))
-                    // console.log(Math.round(weatherData.current.humidity))
-                    // console.log(Math.round(weatherData.current.wind_speed))
-                    // console.log()
-                    // ----------- FUTURE
-
-        for (i=1; i<6; i++) {
-            $("#future-date"+i).text(moment.unix(weatherData.daily[i].dt).format("M/DD"))
-            $("#future-icon"+i)
-            $("#future-temp"+i).text("Temperature: " + (Math.round(weatherData.daily[i].temp.day)) + "°F")
-            $("#future-humidity"+i).text("Humidity: " + (Math.round(weatherData.daily[i].humidity)) + "%")
+function renderWeather (weatherData, cityName, stateName) {
+    // Display current weather data.
+    // debugger;
+    $("#current-weather").text(cityName +", " + stateName + " - " + (moment.unix(weatherData.current.dt).format("M/DD")))
+    var currentIcon = weatherData.current.weather[0].icon;
+    $("#current-icon").attr("src", "https://openweathermap.org/img/w/" + currentIcon + ".png");
+    $("#current-temp").text("Temperature: " + (Math.round(weatherData.current.temp)) + "°F")
+    $("#current-humidity").text("Humidity: " + (Math.round(weatherData.current.humidity)) + "%")
+    $("#wind-speed").text("Wind Speed: " + (Math.round(weatherData.current.wind_speed)) + " Mph")
+    $("#uv-index").text("UV Index: "+(Math.round(weatherData.current.uvi)))
+    // Styles UV index background color based on index value
+    if (weatherData.current.uvi < 4) {
+        $("#uv-index").addClass("low-uv").removeClass("mid-uv high-uv");
+    } else if (weatherData.current.uvi < 7) {
+        $("#uv-index").addClass("mid-uv").removeClass("low-uv high-uv");
+    } else {
+        $("#uv-index").addClass("high-uv").removeClass("low-uv mid-uv");
+    }
+    // Display 5 day future forcast.
+    for (i=1; i<6; i++) {
+        $("#future-date"+i).text(moment.unix(weatherData.daily[i].dt).format("M/DD"))
+        var futureIcon = weatherData.daily[i].weather[0].icon;
+        $("#future-icon"+[i]).attr("src", "https://openweathermap.org/img/w/" + futureIcon + ".png");
+        $("#future-temp"+i).text("Temperature: " + (Math.round(weatherData.daily[i].temp.day)) + "°F")
+        $("#future-humidity"+i).text("Humidity: " + (Math.round(weatherData.daily[i].humidity)) + "%")
         }
-
-
-
-
-
-        });
-    });
 };
-
-
-
-
-// function misc() {
-// }
-
-// displayWeather() {
-    // console.log(weatherData[0].current.temp)
-// }
-
-
-
-
-
-
-
-
-
-
 
 
 
 
 // localStorage.setItem(cityInputValue, weatherData)
-
-//When a search history item is clicked, either run displayWeather using value, or pull from local storage and parse into weatherData.
+//When a search history item is clicked, either run getWeather using value, or pull from local storage and parse into weatherData.
 
 // Math.round()  to round temps or other variables
 // &exclude={part}
